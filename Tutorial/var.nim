f684dfb0b065e06dd721c9fde2d7b7294117a9cb
axect <edeftg@gmail.com>
2018-04-18 21:47:09 +0900

    Fail

---
+++ Tutorial/var.nim
@@ -0,0 +1,8 @@
+const 
+    x = 3 
+    y = 3
+
+var a,b,c = "hi"
+
+echo "x+y ", x+y
+echo "a,b,c ", a, b, c
---
+++ math_test.nim
@@ -0,0 +1,7 @@
+import math
+import random, sequtils
+
+var giga = 2 ^ 9
+var a = shuffle(toSeq(1..10))
+
+echo "seqNumber ", a
